aa4b1918e5015906fab910af3609de33690c899e
291564.umk@gmail.com <291564.umk@gmail.com>
2021-07-29 15:10:26 +0200

    Code refactor

--- blog-forum/src/app/layout/layout.module.ts
+++ blog-forum/src/app/layout/layout.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LeftColumnComponent } from './components/left-column/left-column.component';
-import { RightColumnComponent } from './components/right-column/right-column.component';
 import { ContentComponent } from './components/content/content.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 const components = [
-  LeftColumnComponent,
-  RightColumnComponent,
   ContentComponent
 ];
 
